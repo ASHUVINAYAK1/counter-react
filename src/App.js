41d1b6969a6d1637657eed433bbf7c5214099551
ashuvinayak1508 <ashuvinayak1508@gmail.com>
2023-12-10 14:49:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  const [count,setCount] = useState(0);
+  function addHandler(){
+    setCount(count+1);
+    console.log(count)
+  }
+  function subHandler(){
+    setCount(count-1);
+    console.log(count)
+  }
+  function resetHandler(){
+    setCount(0)
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='heading'>increment and decrement</div>
+      <div className='counter'>
+        <button onClick={subHandler}>
+          -
+        </button>
+
+        <div className='num'>
+          {count}
+        </div>
+
+        <button onClick={addHandler}>
+          +
+        </button>
+
+      </div>
+      <button className='button' onClick={resetHandler}>reset</button>
     </div>
   );
 }
